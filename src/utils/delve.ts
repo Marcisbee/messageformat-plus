d8385c5fa93d1974ab3c021f99997c057967002a
Marcis Bergmanis <marcisbergmanis@gmail.com>
2025-09-19 15:05:30 +0300

    format + lint

--- src/utils/delve.ts
+++ src/utils/delve.ts
@@ -1,12 +1,12 @@
 export default function dlv(
-	obj: Record<string, any> | undefined,
-	key: Array<string | number>,
-	def?: any,
-	p?: number,
-	undef?: any,
+  obj: Record<string, any> | undefined,
+  key: Array<string | number>,
+  def?: any,
+  p?: number,
+  undef?: any,
 ) {
-	for (p = 0; p < key.length; p++) {
-		obj = obj ? obj[key[p]] : undef;
-	}
-	return obj === undef ? def : obj;
+  for (p = 0; p < key.length; p++) {
+    obj = obj ? obj[key[p]] : undef;
+  }
+  return obj === undef ? def : obj;
 }
